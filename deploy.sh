f32e95db198b47224d13a28aa021ebbabe788a80
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2022-09-03 18:23:22 -0500

    ADD: configuracion para despliegue

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:maximopeoficiales/barcode-dvuelve-r.git main:gh-pages
+git push -f git@github.com:maximopeoficiales/react-ts-tesis-proyecto-salud.git main:gh-pages
 
 cd -
