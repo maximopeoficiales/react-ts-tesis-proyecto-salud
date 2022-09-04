972251ff9e37cda46511eda23ac184816da63169
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2022-09-03 22:39:01 -0500

    ADD: button emoticon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
-import { Nav } from "@ui/components/Nav";
+import { Nav } from "@ui/components/Nav/Nav";
 
 export const Index = () => {
     return (
         <div>
             <Nav />
-
             <h1>hola mundo</h1>
         </div>
     )
